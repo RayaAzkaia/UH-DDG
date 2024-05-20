4d7f717c1a5ce18835b1e23f96486e369524613f
unknown <rayaadista54@gmail.com>
2024-05-20 05:53:39 -0700

    first commit

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class = "nav">
+        <a href="dashboard.php">Home</a>
+        <a href="Data_buku.php">Data buku</a>
+        <a href="Data_pengguna.php">Data pengguna</a>
+
+    </div>
+
+    <div class = "content">
+        <h1>Ini Home</h1>
+    </div>
+    
+</body>
+</html>
